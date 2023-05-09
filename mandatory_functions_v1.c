1dedf6ac1d341d5708571a53bd56a0df5ec10d23
jeremias <jeremiaserba7894@gmail.com>
2023-05-09 12:22:28 -0700

    Developed of functions | _push & _pall

--- mandatory_functions_v1.c
+++ mandatory_functions_v1.c
@@ -7,9 +7,34 @@
 * @line: number of lines.
 */
 
-void _push(stack_t **stack, unsigned int line)
+void _push(stack_t **stack, unsigned int line_number)
 {
-    printf("Entro _push\n");
+    stack_t *newNode = NULL;
+    
+    newNode = malloc(sizeof(stack_t));
+    if (!newNode)
+    {
+        write(STDERR_FILENO, "Error: malloc failed\n", 21);
+        exit(EXIT_FAILURE);
+    }
+    if (!stack)
+    {
+        newNode->n = line_number;
+        newNode->prev = NULL;
+        newNode->next = NULL;
+        *stack = newNode;
+    }
+    else
+    {
+        newNode->n = line_number;
+        newNode->prev = NULL;
+        newNode->next = *stack;
+        if (*stack)
+        {
+            (*stack)->prev = newNode;
+        }
+        *stack = newNode;
+    }
 }
 
 /**
@@ -19,9 +44,19 @@ void _push(stack_t **stack, unsigned int line)
 * @line: number of lines.
 */
 
-void _pall(stack_t **stack, unsigned int line)
+void _pall(stack_t **stack, unsigned int __attribute__((unused)) line_number)
 {
-    printf("Entro _pall\n");
+    stack_t *temp = NULL;
+
+	if (*stack)
+	{
+		while (*stack != NULL)
+		{
+            if ((*stack)->n != 0)
+			    printf("%d\n", (*stack)->n);
+			*stack = (*stack)->next;
+		}
+	}
 }
 
 /**
