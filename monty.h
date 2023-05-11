562f29c0da13506d10aab5f93ebf9ca763f7485f
Sami <5741@holbertonstudents.com>
2023-05-11 08:48:52 -0700

    function pop

--- monty.h
+++ monty.h
@@ -47,7 +47,7 @@ typedef struct instruction_s
 void _push(stack_t **, unsigned int);
 void _pall(stack_t **, unsigned int);
 void _pint(stack_t **stack, unsigned int line_number);
-void _pop(stack_t **, unsigned int);
+void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **, unsigned int);
 void _add(stack_t **, unsigned int);
 void _nop(stack_t **stack, unsigned int line_number);
